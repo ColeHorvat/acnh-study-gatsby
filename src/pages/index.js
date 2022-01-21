f51da446c2e337302a8e8ac195ad6cbc27f504a3
Cole Horvat <ColeHorvat@gmail.com>
2022-01-21 12:17:59 -0800

    Added threejs, canvas, and test element

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,19 @@
 import * as React from "react"
 import Container from '../components/Container'
+import Box from '../components/Box'
 import Header from '../components/Header'
+import { Canvas } from "@react-three/fiber"
 
 const IndexPage = () => {
   return (
     <Container>
         <Header />
+        <Canvas>
+            <ambientLight />
+            <pointLight position={[10, 10, 10]} />
+            <Box position={[-1, 0, 0]} />
+            <Box position={[1, 0, 0]} />
+        </Canvas>
     </Container>
   )
 }
