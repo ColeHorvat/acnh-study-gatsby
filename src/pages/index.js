314cd605da0033567ffebca90e825cb78eda9106
Cole Horvat <ColeHorvat@gmail.com>
2022-01-21 14:09:21 -0800

    Added fetch for background music

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,17 @@ import * as React from "react"
 import Container from '../components/Container'
 import Header from '../components/Header'
 
+fetch("http://acnhapi.com/v1/backgroundmusic/", {
+    "method": "GET",
+    "headers": {}
+    })
+    .then(response => {
+    console.log(response.json());
+    })
+    .catch(err => {
+    console.error(err);
+    });
+
 const IndexPage = () => {
   return (
     <Container>
